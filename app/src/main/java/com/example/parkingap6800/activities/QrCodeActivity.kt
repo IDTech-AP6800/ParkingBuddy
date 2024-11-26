af5ff23ca08ba5f181bef7ad55ef8e1e61edd5e2
Sanika Bhamare <sanikabhamare@gmail.com>
2024-11-25 17:50:38 -0800

    Kotlin class for qr code page

---
+++ app/src/main/java/com/example/parkingap6800/activities/QrCodeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.parkingap6800.activities
+
+import android.os.Bundle
+import com.example.parkingap6800.R
+import androidx.appcompat.app.AppCompatActivity
+
+
+class QrCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tap)
+
+        // Initialize the NavigationBar class to handle the navigation bar functionality
+        NavigationBar(this)
+    }
+
+}
