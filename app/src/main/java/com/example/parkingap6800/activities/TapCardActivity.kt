ba2cfd3d1edba6bf67e567fba5ef8f4ec66011dd
Sanika Bhamare <sanikabhamare@gmail.com>
2024-11-25 23:07:27 -0800

    Total due dynamically updates on Tap Page

--- app/src/main/java/com/example/parkingap6800/activities/TapCardActivity.kt
+++ app/src/main/java/com/example/parkingap6800/activities/TapCardActivity.kt
@@ -1,8 +1,10 @@
 package com.example.parkingap6800.activities
 
 import android.os.Bundle
+import android.widget.TextView
 import com.example.parkingap6800.R
 import androidx.appcompat.app.AppCompatActivity
+import com.example.parkingap6800.ParkingSession
 
 
 class TapCardActivity : AppCompatActivity() {
@@ -12,6 +14,10 @@ class TapCardActivity : AppCompatActivity() {
 
         // Initialize the NavigationBar class to handle the navigation bar functionality
         NavigationBar(this)
+
+        val totalDue = ParkingSession.totalDue
+        val totalDueTextView = findViewById<TextView>(R.id.totalDue)
+        totalDueTextView.text = "Total due: $$totalDue"
         }
 
 }
