5ed4c1e27c9b3beff1878cff9d1bafccede4f584
Santiago Palomares <117400795+santiagopalomares@users.noreply.github.com>
2024-12-05 22:33:05 -0800

    30 transition if idle on PaymentSuccess

--- app/src/main/java/com/example/parkingap6800/activities/PaymentSuccessActivity.kt
+++ app/src/main/java/com/example/parkingap6800/activities/PaymentSuccessActivity.kt
@@ -1,6 +1,9 @@
 package com.example.parkingap6800.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.parkingap6800.R
 
@@ -12,5 +15,16 @@ class PaymentSuccessActivity : AppCompatActivity() {
 
         // Initialize the NavigationBar class to handle the navigation bar functionality
         NavigationBar(this)
+
+        // Navigate to MainActivity after 30 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateToMainActivity()
+        }, 30000) // 30000 milliseconds = 30 seconds
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish() // Close PaymentSuccessActivity
     }
 }
