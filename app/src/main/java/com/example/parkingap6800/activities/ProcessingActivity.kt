611203f9757e7de1ba5474dfec83173535e9574e
Santiago Palomares <117400795+santiagopalomares@users.noreply.github.com>
2024-12-05 22:23:10 -0800

    transition after 3 seconds of processing time

--- app/src/main/java/com/example/parkingap6800/activities/ProcessingActivity.kt
+++ app/src/main/java/com/example/parkingap6800/activities/ProcessingActivity.kt
@@ -1,10 +1,12 @@
 package com.example.parkingap6800.activities
+
 import android.content.Intent
 import com.example.parkingap6800.R
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
 
 class ProcessingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +15,19 @@ class ProcessingActivity : AppCompatActivity() {
 
         // Set an OnClickListener on the root view to detect clicks anywhere on the screen
         val rootView = findViewById<View>(android.R.id.content)
-        rootView.setOnClickListener { // Intent to switch to PaymentSuccess activity
-            val intent = Intent(this@ProcessingActivity, PaymentSuccessActivity::class.java)
-            startActivity(intent)
-
-            // Call finish() to close ProcessingActivity
-            finish()
+        rootView.setOnClickListener {
+            navigateToPaymentSuccess()
         }
+
+        // Automatically navigate to PaymentSuccessActivity after 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateToPaymentSuccess()
+        }, 3000) // 3000 milliseconds = 3 seconds
+    }
+
+    private fun navigateToPaymentSuccess() {
+        val intent = Intent(this@ProcessingActivity, PaymentSuccessActivity::class.java)
+        startActivity(intent)
+        finish() // Close ProcessingActivity
     }
 }
