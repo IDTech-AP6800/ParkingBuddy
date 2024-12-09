e080bd7805d4f624d1b16d44980c58359a4167ae
SPerezdeTudela <sperezde@uci.edu>
2024-12-09 14:56:48 -0800

    Added camera preview functionality

--- app/src/main/java/com/example/parkingap6800/activities/QrCodeActivity.kt
+++ app/src/main/java/com/example/parkingap6800/activities/QrCodeActivity.kt
@@ -1,18 +1,38 @@
 package com.example.parkingap6800.activities
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import com.example.parkingap6800.R
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import com.example.parkingap6800.ParkingSession
 
 
+// Code from the Android Studio CameraX tutorial is used here as well
+// as references to SDK tutorial
 class QrCodeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_code)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+            Log.e(TAG, "All Permissions Granted")
+        } else {
+            requestPermissions()
+        }
 
         // Initialize the NavigationBar class to handle the navigation bar functionality
         NavigationBar(this)
@@ -33,6 +53,49 @@ class QrCodeActivity : AppCompatActivity() {
             val intent = Intent(this@QrCodeActivity, ProcessingActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        {
+                permissions ->
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(baseContext, "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            }
+            else {
+                startCamera()
+            }
+        }
+    private fun startCamera() {
+        val cameraController = LifecycleCameraController(baseContext)
+        cameraController.bindToLifecycle(this)
+
+        val previewView: PreviewView = findViewById(R.id.viewFinder)
+        previewView.setController(cameraController)
+
+        cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    }
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val TAG = QrCodeActivity::class.qualifiedName
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA
+            ).toTypedArray()
     }
 
 }
