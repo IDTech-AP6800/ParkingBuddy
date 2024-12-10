99540a44fe35d8caac7a39f2b352c196089c3f2a
Sanika Bhamare <sanikabhamare@gmail.com>
2024-12-10 14:04:16 -0800

    fixed auto timeout, connected no receipt option to homepage, fixed insert card animation

--- app/src/main/java/com/example/parkingap6800/activities/ReceiptSentActivity.kt
+++ app/src/main/java/com/example/parkingap6800/activities/ReceiptSentActivity.kt
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.example.parkingap6800.R
 
 class ReceiptSentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receipt_sent)
+
+        // Apply scale animation the green glow
+        val scaleAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.scale_up_down)
+        val ellipseGlow = findViewById<ImageView>(R.id.ellipse_glow)
+        ellipseGlow.startAnimation(scaleAnim)
 
         // Navigate to MainActivity after 30 seconds
         Handler(Looper.getMainLooper()).postDelayed({
